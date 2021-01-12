737d2c0ad97b45c42c130fe3612d282c51b79c3c
Majestic <pro.majestic.gaming@gmail.com>
2021-01-12 16:51:23 +0100

    2

---
+++ Day_7/exo_03.rb
@@ -0,0 +1,4 @@
+puts "Quelle est votre annee de naissance ?"
+print ">"
+birth = gets.chomp.to_i
+puts "En 2017 vous aviez : #{2017 - birth} ans !"
---
+++ Day_7/exo_4.rb
@@ -0,0 +1,4 @@
+puts "Quelle est votre annee de naissance ?"
+print ">"
+birth = gets.chomp.to_i
+puts "Vous aurez 100 ans en : #{birth + 100} !"
